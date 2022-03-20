68ee07dc2bcc0970cb54e7bf6688f01be34d4ec9
alphak3y <alphamik80@gmail.com>
2022-03-20 17:35:16 -0500

    add amplify config

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+
 function MyApp({ Component, pageProps }) {
+  Amplify.configure({ ...awsExports, ssr: true });
   return <Component {...pageProps} />
 }
 
